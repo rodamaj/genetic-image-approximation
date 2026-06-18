84ffc15731acaef011baa0d448785584f12463ae
Josue Amador <bjamadorr@gmail.com>
2026-06-17 22:12:30 -0600

    remove active pixel indicator

--- js/ui/renderer.js
+++ js/ui/renderer.js
@@ -1,16 +1,9 @@
-import { BACKGROUND_COLOR, HIGHLIGHT_STROKE_COLOR } from "../config/constants.js";
+import { BACKGROUND_COLOR } from "../config/constants.js";
 
 function drawFigure(p, figure) {
   p.noStroke();
   p.fill(figure.color.r, figure.color.g, figure.color.b, figure.color.a);
   p.square(figure.x, figure.y, figure.size);
-
-  if (figure.isSelected) {
-    p.noFill();
-    p.stroke(HIGHLIGHT_STROKE_COLOR);
-    p.strokeWeight(2);
-    p.square(figure.x + 1, figure.y + 1, figure.size - 2);
-  }
 }
 
 export function redrawPopulation(p, population) {
